024eeea8dab8d7e734a6ce49ad308269228c2d4c
storofsoft <storyofsoft1024@gmail.com>
2021-08-02 21:46:00 +0100

    gem_12

--- src/components/WalletProviderModal/DefiConnect.js
+++
@@ -1,9 +0,0 @@
-import Web3 from 'web3'
-import { DeFiWeb3Connector } from 'deficonnect'
-
-
-export const connector = new DeFiWeb3Connector({
-  supportedChainIds: [25],
-  rpc: { 25: 'https://cronosrpc-1.xstaking.sg/'},
-  pollingInterval: 15000,
-})
